88108f1f8146bc97540ef75668bab15a832ad4e6
kimchanghee <changhee94@ajou.ac.kr>
2018-12-15 17:22:34 +0900

    remove directory

--- opensw/docsmsft.docs-article-templates-0.1.3/out/helper/user-metadata.js
+++
@@ -1,8 +0,0 @@
-"use-strict";
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-exports.gitHubID = vscode.workspace.getConfiguration("docs.templates").githubid;
-exports.alias = vscode.workspace.getConfiguration("docs.templates").alias;
-exports.missingValue = "NO VALUE SET";
-//# sourceMappingURL=user-metadata.js.map
